63e5ccca7d7500230b3136f314c5be3ef7049d62
bmanjuu <bettyofung@gmail.com>
2016-06-01 10:41:53 -0400

    second pass at palindrome detector

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -7,20 +7,91 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    /**
-     
-     * Write your check code here.
-     
-     */
+    NSString *palindrome = @"palindrome";
+    NSString *reversed = [self stringByReversingString:palindrome];
+    NSLog(@"%@ : %@", palindrome, reversed);
+    
+    NSString *racecar = @"racecar";
+    BOOL racecarIsPalindrome = [self stringIsPalindrome:racecar];
+    NSLog(@"%d : %@", racecarIsPalindrome, racecar);
+    
+    BOOL palindromeIsPalindrome = [self stringIsPalindrome:palindrome];
+    NSLog(@"%d : %@", palindromeIsPalindrome, palindrome);
+    
+    NSString *bob = @"Bob";
+    BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
+    NSLog(@"%d : %@", bobIsPalindrome, bob);
+    
+    NSString *kanakanak = @"Kanakanak";
+    BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
+    NSLog(@"%d : %@", kanakanakIsPalindrome, kanakanak);
+    
+    NSString *aibohphobia = @"Aibohphobia";
+    BOOL aibohphobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
+    NSLog(@"%d : %@", aibohphobiaIsPalindrome, aibohphobia);
+    
+    NSString *notAPalindrome = @"this is not a palindrome";
+    BOOL notAPalindromeIsPalindrome = [self stringIsPalindrome:notAPalindrome];
+    NSLog(@"%d : %@", notAPalindromeIsPalindrome, notAPalindrome);
+    
+    NSString *neverOdd = @"never odd or even";
+    BOOL neverOddIsPalindrome = [self stringIsPalindrome:neverOdd];
+    NSLog(@"%d : %@", neverOddIsPalindrome, neverOdd);
+    
+    NSString *iPreferPi = @"I prefer pi";
+    BOOL iPreferPiIsPalindrome = [self stringIsPalindrome:iPreferPi];
+    NSLog(@"%d : %@", iPreferPiIsPalindrome, iPreferPi);
+    
+    NSString *fleeToMe = @"Flee to me, remote elf.";
+    BOOL fleeToMeIsPalindrome = [self stringIsPalindrome:fleeToMe];
+    NSLog(@"%d : %@", fleeToMeIsPalindrome, fleeToMe);
+    
+    NSString *norma = @"Norma is as selfless as I am, Ron.";
+    BOOL normaIsPalindrome = [self stringIsPalindrome:norma];
+    NSLog(@"%d : %@", normaIsPalindrome, norma);
+    
+    NSString *papayaWar = @"No sir! Away! A papaya war is on.";
+    BOOL papayaWarIsPalindrome = [self stringIsPalindrome:papayaWar];
+    NSLog(@"%d : %@", papayaWarIsPalindrome, papayaWar);
     
     // do not alter
     return YES;  //
 }   ///////////////
 
-/**
- 
- * Implement your methods here.
- 
- */
+-(BOOL)stringIsPalindrome:(NSString *)string{
+    //steps to take:    1. get the reverse of the string you're testing
+    //                  2. compare original string to reverse
+    //                  3. return result of comparison (BOOL)
+    
+    NSArray *punctuations = @[@",", @".", @"!", @"?", @":", @";"];
+    NSString *withoutPunctuation = [string copy];
+    
+    for(NSUInteger i = 0; i < [punctuations count]; i++){
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuations[i] withString:@""];
+    }
+    
+    NSString *lowercase = [withoutPunctuation lowercaseString];
+    NSString *spaceless = [lowercase stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    //have to make sure you're using the right recipient object for each of the methods above that alter the string argument, you should continue modifying the previously modified string and then take the last method to pass into reverse and check if its a palindrome 
+    
+    NSString *reverse = [self stringByReversingString:spaceless];
+    BOOL stringIsEqualToReverse = [spaceless isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+
+-(NSString *)stringByReversingString:(NSString *)string{
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    
+    return result;
+}
 
 @end
